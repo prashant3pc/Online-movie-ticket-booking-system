10e335c86bab86ac1b5ba0920894242bbd1b81ce
prashant3pc <chaudharypuspa502@gmail.com>
2026-09-24 01:51:31 +0545

    user auth completed

---
+++ backend/controllers/userController.js
@@ -0,0 +1,43 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/User";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({ name, email, password: hashedPassword });
+  return res.status(201).json({
+    success: true,
+    message: "Registration Successful",
+    data: user,
+  });
+});
+
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+  const comparePassword = await bcrypt.compare(password, user.password);
+  if (!comparePassword) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid credential",
+    });
+  }
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+  res.json({
+    success: true,
+    message: "token created",
+    data: token,
+  });
+});
